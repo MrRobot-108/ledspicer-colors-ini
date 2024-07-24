202289202a30a4c2161c3e3bf4699ceb2dc69b62
MrRobot-108 <137833073+MrRobot-108@users.noreply.github.com>
2024-07-24 22:15:35 +0200

    Update start.sh
    deleted useless echo

--- Scripts/RCADE/game-start/start.sh
+++ Scripts/RCADE/game-start/start.sh
@@ -8,6 +8,4 @@ system=$(echo "$romPath" | awk -F'/roms/' '{print $2}' | cut -d'/' -f1)
 
 if grep -q "\\[$romName\\]" /rcade/share/configs/ledspicer/colors.ini;
 then system="arcade"; fi
-echo "$system" >> /rcade/share/start.txt
-echo "$romName" >> /rcade/share/start.txt
 emitter -r -c /rcade/share/configs/ledspicer/ledspicer.conf LoadProfileByEmulator "$romName" "$system"
